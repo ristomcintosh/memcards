3569856f342d13b4dc666211e32a75b0665b788b
Risto Mcintosh <risto.mcintosh@gmail.com>
2024-05-25 16:40:49 -0500

    add create flashcard page

---
+++ src/app/create-flashcard/page.tsx
@@ -0,0 +1,61 @@
+import { Field, Label, Input, Select, Button } from "@headlessui/react"
+
+export default function CreateFlashcard() {
+  return (
+    <div className="flex flex-col items-center py-4">
+      <h1 className="mb-6 text-3xl">Create A New Flashcard</h1>
+      <div className="w-full max-w-xs">
+        <Form />
+      </div>
+    </div>
+  )
+}
+
+const Form = () => {
+  const handleSubmit = async (formData: FormData) => {
+    "use server"
+    console.log(formData)
+  }
+
+  return (
+    <form action={handleSubmit}>
+      <Field>
+        <Label htmlFor="deck" className="sr-only">
+          Deck
+        </Label>
+        <Select
+          id="deck"
+          name="deckName"
+          className="w-full mb-5 text-xl py-1 rounded"
+        >
+          <option value="deck 1">Deck 1</option>
+          <option value="deck 2">Deck 2</option>
+        </Select>
+      </Field>
+
+      <TextInput label="Front" name="front" />
+
+      <TextInput label="Back" name="back" />
+
+      <Button
+        type="submit"
+        className="px-4 mt-4 text-2xl md:text-xl h-12 w-full rounded whitespace-nowrap shadow py-1 no-underline md:h-auto bg-brand-600 text-white"
+      >
+        Create
+      </Button>
+    </form>
+  )
+}
+
+const TextInput = ({ label, name }: { label: string; name: string }) => {
+  return (
+    <Field>
+      <Label className="block mb-2 text-base">{label}</Label>
+      <Input
+        type="text"
+        name={name}
+        className="w-full mb-5 text-xl py-1 rounded md:h-auto"
+      />
+    </Field>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,91 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      maxWidth: {
+        xxs: '15rem',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: colors.black,
+      gray: {
+        100: '#F3F5F4',
+        200: '#E8EBEB',
+        300: '#C1C5C5',
+        400: '#898C8C',
+        500: '#3E4040',
+        600: '#2D3637',
+        700: '#1F2B2E',
+        800: '#132025',
+        900: '#0B181E',
+      },
+      brand: {
+        100: '#CEFAEF',
+        200: '#9EF6E7',
+        300: '#6AE4DA',
+        400: '#43C9C9',
+        500: '#139AA6',
+        600: '#0D798E',
+        700: '#095C77',
+        800: '#064260',
+        900: '#03304F',
+      },
+      info: {
+        100: '#CEEBFD',
+        200: '#9ED3FB',
+        300: '#6DB4F4',
+        400: '#4896EA',
+        500: '#116ADD',
+        600: '#0C52BE',
+        700: '#083C9F',
+        800: '#052A80',
+        900: '#031D6A',
+      },
+      warning: {
+        100: '#FEF5CB',
+        200: '#FDE898',
+        300: '#FAD664',
+        400: '#F5C33D',
+        500: '#EFA700',
+        600: '#CD8900',
+        700: '#AC6D00',
+        800: '#8A5300',
+        900: '#724100',
+      },
+      danger: {
+        100: '#FDE3D1',
+        200: '#FCC0A4',
+        300: '#F79475',
+        400: '#EF6A52',
+        500: '#E52B1D',
+        600: '#C41517',
+        700: '#A40E1C',
+        800: '#84091F',
+        900: '#6D0520',
+      },
+      success: {
+        100: '#EEFBD2',
+        200: '#D9F8A7',
+        300: '#B9EC79',
+        400: '#98D954',
+        500: '#6BC124',
+        600: '#51A51A',
+        700: '#3B8A12',
+        800: '#286F0B',
+        900: '#1B5C06',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('@tailwindcss/forms')],
+}
+export default config
