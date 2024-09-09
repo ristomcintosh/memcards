6ea75bedb98eb02a76faf72e4bca3c35c67a3269
Risto Mcintosh <risto.mcintosh@gmail.com>
2024-09-08 21:16:42 -0500

    create stories for all button variants

--- src/components/ui/button/button.stories.tsx
+++ src/components/ui/button/button.stories.tsx
@@ -1,16 +1,78 @@
 import { Meta, StoryObj } from "@storybook/react"
 
-import { Button, ButtonProps } from "./button"
+import { Button } from "./button"
+import { Edit } from "lucide-react"
 
-const meta = {
+const meta: Meta<typeof Button> = {
   title: "Button",
   component: Button,
   args: {
     children: "Button",
+    size: "default",
+    disabled: false,
   },
-} satisfies Meta<ButtonProps>
+  argTypes: {
+    variant: {
+      control: "select",
+      options: [
+        "default",
+        "destructive",
+        "outline",
+        "secondary",
+        "ghost",
+        "link",
+      ],
+    },
+    size: {
+      control: "select",
+      options: ["default", "sm", "lg", "icon"],
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
 
 export default meta
-type Story = StoryObj<ButtonProps>
+type Story = StoryObj<typeof Button>
 
 export const Default: Story = {}
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+  },
+}
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+  },
+}
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+  },
+}
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+  },
+}
+
+export const Link: Story = {
+  args: {
+    variant: "link",
+  },
+}
+
+export const IconButton: Story = {
+  args: {
+    children: <Edit />,
+    size: "icon",
+  },
+}
