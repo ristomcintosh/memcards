6a044d7fb3e8f92d895dc5ec57df9a1e7bd39d11
Risto Mcintosh <risto.mcintosh@gmail.com>
2024-09-15 22:24:50 -0500

    remove tests file

--- src/components/interfaces/study/__tests__/Completed.test.tsx
+++ src/components/interfaces/study/__tests__/Completed.test.tsx
@@ -1,23 +1,41 @@
 import { render, screen } from "@testing-library/react"
 import { Completed } from "../Completed"
-import { decks } from "@/tests/testData"
+import { DeckWithFlashcards } from "@/types"
+
+const deck = {
+  id: "1",
+  name: "Deck 1",
+  userId: "user-id",
+  flashcards: [
+    {
+      id: "some-id",
+      deckId: "some-id",
+      front: "front",
+      back: "back",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ],
+  createdAt: new Date(),
+  updatedAt: new Date(),
+} satisfies DeckWithFlashcards
 
 describe(Completed.name, () => {
   it("renders", () => {
-    render(<Completed restart={jest.fn()} deck={decks[0]} />)
+    render(<Completed restart={jest.fn()} deck={deck} />)
 
     expect(screen.getByText("Congratulations! 🎉")).toBeInTheDocument()
     expect(
       screen.getByText(
-        "You have successfully completed all the flashcards in this deck."
-      )
+        "You have successfully completed all the flashcards in this deck.",
+      ),
     ).toBeInTheDocument()
     expect(screen.getByText("Restart")).toBeInTheDocument()
     expect(screen.getByText("Home")).toBeInTheDocument()
   })
 
   it("does not show the restart button when there are no cards left in deck", () => {
-    const deckWithNoCards = { ...decks[0], flashcards: [] }
+    const deckWithNoCards = { ...deck, flashcards: [] }
     render(<Completed restart={jest.fn()} deck={deckWithNoCards} />)
 
     expect(screen.queryByText("Restart")).not.toBeInTheDocument()
