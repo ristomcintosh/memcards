b57cb1fac13303ee4ea64012bcd09f06eecff8d0
Risto Mcintosh <risto.mcintosh@gmail.com>
2024-09-17 15:40:23 -0500

    fix suppress hydration warning caused by dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
       <body>
         <Analytics />
         <SetThemeScript />
