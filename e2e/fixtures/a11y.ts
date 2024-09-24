2e018662ad167301745ed3f4a14772da6611a869
Risto Mcintosh <risto.mcintosh@gmail.com>
2024-09-24 00:33:01 -0500

    fix type erros in e2e file

--- e2e/fixtures/a11y.ts
+++ e2e/fixtures/a11y.ts
@@ -1,13 +1,12 @@
 // makeAxeBuilder is a playwright test fixture that returns a function that can be used to run axe tests on a page
-
-import AxeBuilderBase from "@axe-core/playwright"
-import {
+import AxeBuilderBase from "@axe-core/playwright";
+import type {
   PlaywrightTestArgs,
   PlaywrightTestOptions,
   PlaywrightWorkerArgs,
   PlaywrightWorkerOptions,
   TestFixture,
-} from "@playwright/test"
+} from "@playwright/test";
 
 export const makeAxeBuilder: TestFixture<
   () => AxeBuilder,
@@ -16,21 +15,21 @@ export const makeAxeBuilder: TestFixture<
     PlaywrightWorkerArgs &
     PlaywrightWorkerOptions
 > = async ({ page }, use) => {
-  const makeAxeBuilder = () => new AxeBuilder({ page })
-  await use(makeAxeBuilder)
-}
+  const makeAxeBuilder = () => new AxeBuilder({ page });
+  await use(makeAxeBuilder);
+};
 
 export class AxeBuilder extends AxeBuilderBase {
   analyzeWithLogger = async () => {
-    const results = await this.analyze()
+    const results = await this.analyze();
     if (results.violations.length > 0) {
-      console.log("Accessibility violations:", results.violations.length)
-      console.log(results.violations)
+      console.log("Accessibility violations:", results.violations.length);
+      console.log(results.violations);
       console.log(
         "Nodes with violations:",
-        results.violations.map((v) => v.nodes)
-      )
+        results.violations.map((v) => v.nodes),
+      );
     }
-    return results
-  }
+    return results;
+  };
 }
--- e2e/pages/home-page.ts
+++ e2e/pages/home-page.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test"
+import type { Page } from "@playwright/test"
 
 export class HomePage {
   private deckNames = new Set<string>()
